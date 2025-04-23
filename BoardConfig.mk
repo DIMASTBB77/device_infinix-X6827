accc101bcce1179c6fd50cd22155f6e5a165b460
TegarXLu <botdimas67@gmail.com>
2025-04-23 19:53:32 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,6 +102,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Workaround for copying error vendor files to recovery ramdisk
 TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM := system
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
